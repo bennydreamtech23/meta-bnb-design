e7eaa7e0e3cc21db747c4f1b8a1c6e5f2d4e5843
Ijeoma Uwabunkeonye <uwabunkeonyeijeoma@gmail.com>
2022-11-14 20:00:07 +0100

    mediaquery change

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -4,7 +4,7 @@ const Brand = () =>{
   return(
     <div 
     className="grid grid-cols-3 gap-10 px-10 py-5 text-white 
-    md:mt-0 lg:mt-0 -mt-20 md:mt-10"
+    md:mt-0 lg:mt-0 -mt-20 md:-mt-30"
     style={{background:"linear-gradient(90deg, #A02279 11.54%, #A02279 11.45%)"}}>
 <div>
 <img
