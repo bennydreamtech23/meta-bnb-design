5ed51c5cae9524e07b8cc780767c508777f93091
Ijeoma Uwabunkeonye <uwabunkeonyeijeoma@gmail.com>
2022-11-14 19:42:02 +0100

    hero-Img

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -3,7 +3,8 @@
 const Brand = () =>{
   return(
     <div 
-    className="grid grid-cols-3 gap-10 px-10 py-5 text-white md:mt-0 lg:mt-0 -mt-20 md:mt-10"
+    className="grid grid-cols-3 gap-10 px-10 py-5 text-white 
+    md:mt-0 lg:mt-0 -mt-20 md:mt-10"
     style={{background:"linear-gradient(90deg, #A02279 11.54%, #A02279 11.45%)"}}>
 <div>
 <img
